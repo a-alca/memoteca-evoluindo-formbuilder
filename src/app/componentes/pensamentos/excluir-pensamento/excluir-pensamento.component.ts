04358d24691c2be20124e959332bcf45b620183d
André Alcântara <106185003+a-alca@users.noreply.github.com>
2024-03-04 14:11:12 -0300

    files

---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css']
+})
+export class ExcluirPensamentoComponent {
+
+  pensamento: Pensamento = {
+    id: '',
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {}
+
+    ngOnInit(): void {
+      const id =  this.route.snapshot.paramMap.get('id')
+      this.service.buscarPorId(id!).subscribe((pensamento) => {
+        this.pensamento = pensamento
+      })
+    }
+
+    excluirPensamento() {
+      if(this.pensamento.id) {
+        this.service.excluirPensamento(this.pensamento.id).subscribe(() => {
+          this.router.navigate(['/listarPensamento'])
+        })
+      }
+    }
+
+    cancelarPensamento() {
+      this.router.navigate(['/listarPensamento'])
+    }
+}
