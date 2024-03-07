58147b8bdf5871ebc29a7b4b07a6692749b4d5f5
André Alcântara <106185003+a-alca@users.noreply.github.com>
2024-03-07 13:18:19 -0300

    aula 6 - Compartilhando informacoes entres componentes; recarrregar componente de uma pagina com RouteReuseStrategy e customReuseStrategy; ajuste de titulo da pagina de acordo com conteudo acessado

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -12,18 +13,24 @@ export class ListarPensamentoComponent {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
-  filtro: string = ''
+  filtro: string = '';
+  favoritos: boolean = false;
+  listaFavoritos: Pensamento[] = []
+  titulo: string = 'Meu Mural'
 
-  constructor(private service: PensamentoService) { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos)=> {
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos)=> {
       this.listaPensamentos = listaPensamentos
     })
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual, this.filtro)
+    this.service.listar(++this.paginaAtual, this.filtro, this.favoritos)
       .subscribe(listaPensamentos => {
         this.listaPensamentos.push(...listaPensamentos);
         if(!listaPensamentos.length) {
@@ -35,9 +42,30 @@ export class ListarPensamentoComponent {
   pesquisarPensamentos() {
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
-    this.service.listar(this.paginaAtual, this.filtro)
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
       .subscribe(listaPensamentos => {
         this.listaPensamentos = listaPensamentos
+      })
+  }
+
+  recarregarComponente() {
+    // location.reload(); o location.reload() executa o metodo de recarregamento de pagina, o que nao é desejado num projeto SPA. Refatorado na abordagem abaixo com a classe rauter para navegacao entre views
+    this.favoritos = false;
+    this.paginaAtual = 1;
+    // this.router.routeReuseStrategy.shouldReuseRoute = () => false; // routeReuseStrategy DEPRECATED
+    // this.router.onSameUrlNavigation = 'reload'; // onSameUrlNavigation DEPRECATED
+    this.router.navigate([this.router.url])
+  }
+
+  listarFavoritos() {
+    this.titulo = 'Meus favoritos'
+    this.favoritos = true
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe(listaPensamentosFavoritos => {
+        this.listaPensamentos = listaPensamentosFavoritos
+        this.listaFavoritos = listaPensamentosFavoritos
       })
   }
 
